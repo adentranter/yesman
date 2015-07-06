56330cfd225e8d3afaf9279659be952075cdc24c
Aden Tranter <me@adentranter.com>
2015-07-06 14:34:37 +1000

    added new fonts and css to add footer

--- app/models/question.js
+++ app/models/question.js
@@ -8,9 +8,9 @@ var question =  DS.Model.extend({
 
 question.reopenClass({
   FIXTURES: [
-  { id: 1, question: 'Am i getting paid?', yes:5, no:0  },
+  { id: 1, question: 'Am I getting paid?', yes:5, no:0  },
   { id: 2, question: 'Will it take less than an hour including driving time', yes:3, no:0  },
-  { id: 3, question: 'Can i email/give advice instead? ', yes:3, no:0  },
+  { id: 3, question: 'Can  email/give advice instead? ', yes:3, no:0  },
   { id: 4, question: 'Will this help grow my brand/business?', yes:5, no:0  },
   { id: 5, question: 'Is this a family extended related issue? ', yes:3.5, no:0  },
   { id: 6, question: 'Is this a family related issue? ', yes:8, no:0  },
